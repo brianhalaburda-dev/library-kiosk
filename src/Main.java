36663d66a1c685c71cc343f4249dd6630c4c3bbf
Brian <mac@mbp-brian.local>
2025-08-26 13:08:11 -0300

    ArrayList Support. Now it can show multiple Books in a list.

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,60 @@
-// Import the Scanner class from the Java 'utils' library.
-// This is like telling the program, "Go get the Scanner tool for us."
-import java.util.Scanner;
+import java.util.ArrayList; // Import the Array List class from the Java 'utils' library.
+import java.util.Scanner; // Import the Scanner class from the Java 'utils' library.
 
 public class Main {
     public static void main(String[]args) {
+       Scanner scanner = new Scanner(System.in); // Create a Scanner object to read input from the keyboard. System ' System.in' means "standard input", which is the keyboard.
 
-        // Create a Scanner object to read input from the keyboard.
-        // System ' System.in' means "standard input", which is the keyboard.
-        Scanner scanner = new Scanner(System.in);
+        // 1. Create the list.
+        ArrayList<Book> bookList = new ArrayList<>(); // This creates an empty ArrayList that can only hold Book objects. It's created outside the while loops so it persists (isn't reset every loop).
 
-        // This is our main program loop. It will keep showing the menu until we break out.
-        while (true) {
-            // Print the menu options for the user.
+        while(true) {
             System.out.println("\n---Library Kiosk Menu---");
-            System.out.println("1. Add a new book:");
-            System.out.println("2. Exit");
-            System.out.println("Please choose an option (1 or 2): ");
+            System.out.println("1. Add a new book");
+            System.out.println("2. List all books"); // The new option.
+            System.out.println("3. Exit");
+            System.out.println("Please choose an option: (1, 2 or 3): ");
 
-            // Read the User's choice as a number (integer).
             int choice = scanner.nextInt();
             scanner.nextLine();
-            // This 'consumes' the leftover Enter press after the number.
-            // If we don't do this, it causes a problem on the next input.
 
-            // Check what the user chose.
-            if (choice == 1) {
-                // *** Option 1, Add a Book ***
-                System.out.println("\nLet's add a new book!");
+            if (choice == 1){
+                // Option 1. Add a book.
+                System.out.println("\nAdd a new book.");
 
-                // Prompt the user for each piece of info and read their input.
                 System.out.println("Enter book title: ");
-                String title = scanner.nextLine(); //Reads a whole line of text.
-
+                String title = scanner.nextLine();
                 System.out.println("Enter book author: ");
                 String author = scanner.nextLine();
-
-                System.out.println("Enter book ISBN: ");
+                System.out.println("Enter ISBN: ");
                 String isbn = scanner.nextLine();
 
-                // Now, use the input to create a new Book OBJECT.
-                Book newBook = new Book(title, author, isbn);
+                Book newBook = new Book(title, author, isbn); // Puts data inside new Book Object.
+                bookList.add(newBook); // The 'add' method puts the object into the ArrayList.
 
-                // Print out the new book to confirm it worked.
                 System.out.println("Successfully added: " + newBook);
 
-            } else if (choice == 2) {
+            } else if (choice == 2){
+               // Option 2. List all books.
+                System.out.println("\nListing all books.\n");
 
-                // *** Option 2, Exit ***
-                System.out.println("Thanks for using the Library Kiosk. Goodbye!");
-                break; // The 'break' keyword exist the while loop, ending the program.
+                if (bookList.isEmpty()) {
+                    System.out.println("The library is empty. No books found.");
+                } else {
+                    for (Book tempBook : bookList) {
+                        System.out.println(" - " + tempBook);
+                    }
+                }
+
+            } else if (choice == 3){
+                // Option 3. Exit.
+                System.out.println("Thanks for using Library Kiosk. Goodbye.");
+                break;
 
             } else {
-
-                // *** Handle invalid Input ***
-                System.out.println("Invalid Choice. Please try again.");
+                System.out.println("Invalid choice, please try again.");
             }
         }
-        // Close the scanner to avoid resource leaks (good practice).
         scanner.close();
     }
 }
